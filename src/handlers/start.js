b7379cae7aa0c738e6db200de7a1b982b5dbf3ce
thesunnatillo <sunnatillocoder@gmail.com>
2024-12-08 22:20:26 +0500

    user datalarini saqlash

--- src/handlers/start.js
+++ src/handlers/start.js
@@ -1,3 +1,9 @@
+const saveUser = require("./users/save-user")
 module.exports = async (ctx) => {
-    await ctx.reply(`Salom ${ctx.from.first_name}\nKino kodini yuboring!`)
+    const telegram_id = ctx.from.id
+    const name = ctx.from.first_name
+    const username = ctx.from.username
+
+    await saveUser(telegram_id, name, username);
+    await ctx.reply(`Salom ${ctx.from.first_name}\nKino kodini yuboring!`);
 }
